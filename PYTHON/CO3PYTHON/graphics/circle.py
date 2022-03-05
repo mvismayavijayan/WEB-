fbd25add20198d565f5d6f0e3703813a06bc6c13
VISMAYA VIJAYAN M <94914747+mvismayavijayan@users.noreply.github.com>
2022-03-05 20:37:53 +0530

    Delete PYTHON directory

--- PYTHON/CO3PYTHON/graphics/circle.py
+++
@@ -1,5 +0,0 @@
-import math
-def area(r):
-    return math.pi*r*r
-def perimeter(r):
-    return math.pi*2*r
--- PYTHON/CO3PYTHON/graphics/threedgraphics/cuboid.py
+++
@@ -1,4 +0,0 @@
-def area(l,b,h):
-    return l*b
-def perim(l,b,h):
-    return 4*(l+b+h)
